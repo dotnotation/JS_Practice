1982e7649168aaf006590dd117b7debd5a15fc0e
dotnotation <dotinmotionproductions@gmail.com>
2022-01-10 18:25:56 -0500

    setting up and adding data to play around with

---
+++ src/dog.js
@@ -0,0 +1,13 @@
+export class Dog {
+    constructor(id, name, breed) {
+        this.id = id
+        this.name = name
+        this.breed = breed
+    }
+
+    renderDog(){
+        
+    }
+}
+
+Dog.all = []
---
+++ src/review.js
@@ -0,0 +1,12 @@
+export class Review {
+    constructor(id, text) {
+        this.id = id
+        this.text = text
+    }
+
+    renderReview(){
+        
+    }
+}
+
+Review.all = []
