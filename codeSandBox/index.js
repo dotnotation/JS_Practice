761aed6681cc098e1f763eee94adef729575a011
dotnotation <dotinmotionproductions@gmail.com>
2022-01-11 18:50:26 -0500

    added drop down menu to sandBox index

--- codeSandBox/index.js
+++ codeSandBox/index.js
@@ -6,16 +6,40 @@ const dogDiv = document.querySelector(".dogList"); // grab div
 
 const addDogs = () => {
   // loop over array of dogs
+
+  let values = [];
   for (const dogData of dogs) {
     const { id, name, breed, knownFor } = dogData;
     const dog = new Dog(id, name, breed, knownFor); // create dog objects
     Dog.all.push(dog); // add to Dog.all
     dogDiv.insertAdjacentHTML("beforeend", dog.renderDog());
-    dogDiv.addEventListener("click", dog.handleEvent);
+    // dogDiv.addEventListener("click", dog.handleEvent);
+    console.log(dog.name);
+    values.push(dog.name);
+    console.log(values);
   }
+  console.log(values);
+  addDropDown(values);
 };
 
 addDogs(); // invoke function
+
+function addDropDown(values) {
+  console.log("hit drop down", values);
+  const header = document.getElementById("app");
+  const menu = document.createElement("select");
+
+  menu.setAttribute("id", "name-drop-down");
+  values.forEach((name) => {
+    let option = document.createElement("option");
+    option.value = name;
+    option.text = name;
+    menu.appendChild(option);
+  });
+
+  header.append(menu);
+}
+
 
 // const handleEvent = () => {
 //   dogDiv.addEventListener("click", (e) => {
